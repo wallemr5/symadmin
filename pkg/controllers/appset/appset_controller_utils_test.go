ff331e0c3cdde4ab53a9171b76247fab0d04fdb2
peng.chen <peng.chen1@dmall.com>
2020-01-07 11:11:01 +0800

    add version empty judge

--- pkg/controllers/appset/appset_controller_utils_test.go
+++ pkg/controllers/appset/appset_controller_utils_test.go
@@ -11,16 +11,19 @@ type version struct {
 
 func TestMergeVersion(t *testing.T) {
 	r := map[string]version{
-		"":            version{"", ""},
+		"":            version{"  ", "  "},
 		"v1/v2/v3":    version{"v1", "v2/v3"},
 		"v2/v3":       version{"", "v3/v2"},
 		"v1/v2/v3/v4": version{"v2/v3", "v4/v1"},
+		"v1":          version{"v1", "v1"},
+		"v3/v4":       version{"", "/v4/v3"},
+		"v11/v12":     version{"/v12/v11", " "},
 	}
 
 	for expect, input := range r {
 		current := mergeVersion(input.v1, input.v2)
 		if expect != current {
-			t.Logf("input:%+v, expect:%s, current:%s", input, expect, current)
+			t.Errorf("input:%+v, expect:%s, current:%s", input, expect, current)
 		}
 	}
 }
