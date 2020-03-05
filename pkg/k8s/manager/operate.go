2b872f27cd6f171382fdfcb8e875a82219e6fcff
张海东 <haidongdev@gmail.com>
2020-03-05 16:33:22 +0800

    change manager cluster interface

--- pkg/k8s/manager/operate.go
+++ pkg/k8s/manager/operate.go
@@ -3,24 +3,30 @@ package manager
 import (
 	appv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	rls "k8s.io/helm/pkg/proto/hapi/services"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	rlsv2 "k8s.io/helm/pkg/proto/hapi/release"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // BaseCluster ...
 type BaseCluster interface {
-	GetPod(opts *client.ListOptions, clusters ...string) (*corev1.Pod, error)
+	GetOriginKubeCli(clusters ...string) (kubernetes.Interface, error)
+	GetRestConfig(clusters ...string) (*rest.Config, error)
+	GetPod(opts *types.NamespacedName, clusters ...string) (*corev1.Pod, error)
 	GetPods(opts *client.ListOptions, clusters ...string) ([]*corev1.Pod, error)
+	GetNodes(opts *client.ListOptions, clusters ...string) ([]*corev1.Node, error)
 	GetDeployment(opts *client.ListOptions, clusters ...string) ([]*appv1.Deployment, error)
 	GetService(opts *client.ListOptions, clusters ...string) ([]*corev1.Service, error)
 	GetEndpoint(opts *client.ListOptions, clusters ...string) ([]*corev1.Endpoints, error)
 	GetEvent(opts *client.ListOptions, clusters ...string) ([]*corev1.Event, error)
 	RestartPods(opts *client.ListOptions, clusters ...string) error
-	DeletePod(opts *client.ListOptions, clusters ...string) ([]*corev1.Pod, error)
+	DeletePod(opts *types.NamespacedName, clusters ...string) error
 }
 
 // CustomeCluster ...
 type CustomeCluster interface {
 	BaseCluster
-	GetHelmRelease(cluster, appName, group, releaseName, zone string) (*rls.ListReleasesResponse, error)
+	GetHelmRelease(opts map[string]string, clusters ...string) ([]*rlsv2.Release, error)
 }
