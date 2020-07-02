c1fb0b192f4583109148394977ea6b292f74b9d6
peng.chen <peng.chen1@dmall.com>
2020-07-02 18:24:33 +0800

    modify source_labels interface{} to string

--- pkg/controllers/cluster/monitor/monitor.go
+++ pkg/controllers/cluster/monitor/monitor.go
@@ -534,7 +534,7 @@ func (r *reconciler) buildMonitorValues(app *workloadv1beta1.HelmChartSpec) map[
 				},
 				"additionalScrapeConfigs": []map[string]interface{}{
 					{
-						"job_name": "'istio-mesh'",
+						"job_name": "istio-mesh",
 						"kubernetes_sd_configs": []map[string]interface{}{
 							{
 								"role": "endpoints",
@@ -545,57 +545,46 @@ func (r *reconciler) buildMonitorValues(app *workloadv1beta1.HelmChartSpec) map[
 						},
 						"relabel_configs": []map[string]interface{}{
 							{
-								"source_labels": []string{
-									"__meta_kubernetes_service_name",
-									"__meta_kubernetes_endpoint_port_name",
-								},
-								"action": "keep",
-								"regex":  "istio-telemetry;prometheus",
+								"source_labels": `[__meta_kubernetes_service_name, __meta_kubernetes_endpoint_port_name]`,
+								"action":        "keep",
+								"regex":         "istio-telemetry;prometheus",
 							},
 						},
 					},
 					{
-						"job_name":     "'envoy-stats'",
+						"job_name":     "envoy-stats",
 						"metrics_path": "/stats/prometheus",
 						"kubernetes_sd_configs": []map[string]interface{}{
 							{
 								"role": "pod",
 							},
 						},
-						"relabel_configs": []map[string]interface{}{
+						"relabel_configs": []map[string]string{
 							{
-								"source_labels": []string{
-									"__meta_kubernetes_pod_container_port_name",
-								},
-								"action": "keep",
-								"regex":  "'.*-envoy-prom'",
+								"source_labels": `[__meta_kubernetes_pod_container_port_name]`,
+								"action":        "keep",
+								"regex":         "'.*-envoy-prom'",
 							},
 							{
-								"source_labels": []string{
-									"__address__", "__meta_kubernetes_pod_annotation_prometheus_io_port",
-								},
-								"action":       "replace",
-								"regex":        "([^:]+)(?::\\d+)?;(\\d+)",
-								"replacement":  "$1:15090",
-								"target_label": "__address__",
+								"source_labels": `[__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]`,
+								"action":        "replace",
+								"regex":         `([^:]+)(?::\d+)?;(\d+)`,
+								"replacement":   "$1:15090",
+								"target_label":  "__address__",
 							},
 							{
 								"action": "labeldrop",
 								"regex":  "__meta_kubernetes_pod_label_(.+)",
 							},
 							{
-								"source_labels": []string{
-									"__meta_kubernetes_namespace",
-								},
-								"action":       "replace",
-								"target_label": "namespace",
+								"source_labels": `[__meta_kubernetes_namespace]`,
+								"action":        "replace",
+								"target_label":  "namespace",
 							},
 							{
-								"source_labels": []string{
-									"__meta_kubernetes_pod_name",
-								},
-								"action":       "replace",
-								"target_label": "pod_name",
+								"source_labels": `[__meta_kubernetes_pod_name]`,
+								"action":        "replace",
+								"target_label":  "pod_name",
 							},
 						},
 					},
