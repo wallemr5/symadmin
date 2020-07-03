c60e4e8daf6cdb8288561a496d7e1dfe54976f21
peng.chen <peng.chen1@dmall.com>
2020-07-03 13:20:29 +0800

    change additionalScrapeConfigsStr to json

---
+++ pkg/controllers/cluster/monitor/istio.go
@@ -0,0 +1,34 @@
+package monitor
+
+import (
+	"github.com/ghodss/yaml"
+)
+
+var additionalScrapeConfigsStr = `
+- job_name: envoy-stats
+  metrics_path: /stats/prometheus
+  kubernetes_sd_configs:
+  - role: pod
+  relabel_configs:
+  - source_labels: [__meta_kubernetes_pod_container_port_name]
+    action: keep
+    regex: '.*-envoy-prom'
+  - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
+    action: replace
+    regex: ([^:]+)(?::\d+)?;(\d+)
+    replacement: $1:15090
+    target_label: __address__
+  - action: labeldrop
+    regex: __meta_kubernetes_pod_label_(.+)
+  - source_labels: [__meta_kubernetes_namespace]
+    action: replace
+    target_label: namespace
+  - source_labels: [__meta_kubernetes_pod_name]
+    action: replace
+    target_label: pod_name
+`
+
+func builAadditionalScrapeConfigs() string {
+	b, _ := yaml.YAMLToJSON([]byte(additionalScrapeConfigsStr))
+	return string(b)
+}
--- pkg/controllers/cluster/monitor/monitor.go
+++ pkg/controllers/cluster/monitor/monitor.go
@@ -532,30 +532,7 @@ func (r *reconciler) buildMonitorValues(app *workloadv1beta1.HelmChartSpec) map[
 						},
 					},
 				},
-				"additionalScrapeConfigsExternal": true,
-				"additionalScrapeConfigs": `
-- job_name: envoy-stats
-  metrics_path: /stats/prometheus
-  kubernetes_sd_configs:
-  - role: pod
-  relabel_configs:
-  - source_labels: [__meta_kubernetes_pod_container_port_name]
-    action: keep
-    regex: '.*-envoy-prom'
-  - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
-    action: replace
-    regex: ([^:]+)(?::\d+)?;(\d+)
-    replacement: $1:15090
-    target_label: __address__
-  - action: labeldrop
-    regex: __meta_kubernetes_pod_label_(.+)
-  - source_labels: [__meta_kubernetes_namespace]
-    action: replace
-    target_label: namespace
-  - source_labels: [__meta_kubernetes_pod_name]
-    action: replace
-    target_label: pod_name
-				`,
+				"additionalScrapeConfigs": builAadditionalScrapeConfigs(),
 			},
 		},
 		"grafana": map[string]interface{}{
